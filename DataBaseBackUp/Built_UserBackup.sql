33fe6733d82385dc87c8427a44da19359f035271
P3dr0dSW <pedrodsw@gmail.com>
2018-11-17 21:03:57 +0100

    wrong SQLUser Password

--- DataBaseBackUp/Built_UserBackup.sql
+++ DataBaseBackUp/Built_UserBackup.sql
@@ -1,4 +1,4 @@
 --Creating Webserver User
-create user 'WebServerRoot'@'localhost' identified by 'rooq01';
+create user 'WebServerRoot'@'localhost' identified by 'root01';
 GRANT SELECT,UPDATE,INSERT,DELETE ON ENCRYPTED_CHAT.* To 'WebServerRoot'@'localhost';
 Flush privileges;
