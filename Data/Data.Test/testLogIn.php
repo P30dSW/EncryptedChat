f9659c69d72e288e82a12151e3f04b7f8dcc034f
P30dSW <pedrodsw@gmail.com>
2018-11-27 11:32:12 +0100

    new test functions NOT DEFINED

--- Data/Data.Test/testLogIn.php
+++ Data/Data.Test/testLogIn.php
@@ -1,10 +1,21 @@
 <?php
-include("logInManager.php");
-$usrname = 'testUser01';
+//TODO: better test Functions
+include("../logInManager.php");
+$usrname = 'testUser02';
 $frstname = 'pedro';
 $lastname = 'winkler';
 $eMail = 'go@fuckyourself.com';
 $pswrd = 'pussy';
-$result = registerUser($frstname,$lastname,$usrname,$pswrd,$_email );
-echo $result;
+//------creates User
+//$result = registerUser($frstname,$lastname,$usrname,password_hash($pswrd, PASSWORD_DEFAULT) , $eMail );
+//------check ob den username oder password schon existiert
+// $result = checkUserOrEmailExists($usrname,$eMail);
+// foreach ($result as $key => $val) {
+//     echo $val;
+//  }
+//------check if user exists Tests returns Boolean
+$result = checkUserOrPasswordExists($usrname ,$pswrd );
+ foreach ($result as $key => $val) {
+     echo $val;
+  }
 ?>
